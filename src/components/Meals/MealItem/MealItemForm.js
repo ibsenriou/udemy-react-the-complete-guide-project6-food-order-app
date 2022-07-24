33cf361b2251b26e3a8c73970fa266b6cb3b1ead
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-07-23 21:54:37 -0300

    Initial Commit

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,12 @@
+import classes from "./MealItemForm.module.css";
+
+const MealItem = (props) => {
+  return (
+    <form className={classes.form}>
+      <input />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default MealItem;
